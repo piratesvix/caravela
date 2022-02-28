1386abf2a0250bc14c52970a4721698cfb309298
ahsouza <annibalhsouza@gmail.com>
2022-02-28 19:28:51 -0300

    chore: implementar validações em corpo request

---
+++ src/validators/candidatoRequest.ts
@@ -0,0 +1,11 @@
+import Joi from 'joi';
+
+export const userPOSTSchema = Joi.object()
+  .options({ abortEarly: false })
+  .keys({
+    firstName: Joi.string().min(4).max(125).label('FirstName').required(),
+    lastName: Joi.string().min(4).max(125).label('LastName').required(),
+    name: Joi.string().min(4).max(100).label('Name').required(),
+    email: Joi.string().min(10).max(100).label('Email').required(),
+    password: Joi.string().min(6).max(100).label('Password').required()
+  });
